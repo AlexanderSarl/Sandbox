6be764a59bb4e2fd216826ce14d82d11618600e0
AlexanderSarl <126205899+AlexanderSarl@users.noreply.github.com>
2023-02-24 01:50:29 +0300

    Add files via upload

---
+++ fibonachi.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int number, num1 = 0, num2 = 1;
+    cout << "Введите член ряда фибоначчи:" << endl;
+    cin >> number;
+    cout << endl;
+    for (int i = 0; i < number; i++)
+    {
+        num1 = num1 + num2;        
+        num2 = num1 - num2;        
+    }
+    cout << num1;
+    return 0;
+}
