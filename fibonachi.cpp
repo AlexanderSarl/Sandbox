9f6a7ddad64c207c3d1a9eb49cfcf2d2d4f941ce
AlexanderSarl <alexander.sarleisky@yandex.ru>
2023-03-31 20:52:34 +0300

    get input from command line

--- fibonachi.cpp
+++ fibonachi.cpp
@@ -1,18 +1,16 @@
 ﻿#include <iostream>
-using namespace std;
+#include <cstdlib>
 
-int main()
+
+int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "Rus");
-    int number, num1 = 0, num2 = 1;
-    cout << "Введите член ряда фибоначчи:" << endl;
-    cin >> number;
-    cout << endl;
-    for (int i = 0; i < number; i++)
+    int num1 = 0, num2 = 1;
+    for (int i = 0; i < atoi(argv[1]); i++)
     {
-        num1 = num1 + num2;        
-        num2 = num1 - num2;        
+        num1 = num1 + num2;
+        num2 = num1 - num2;
     }
-    cout << num1;
+    std::cout << num1;
     return 0;
 }
