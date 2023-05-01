59e52f04bc3773cd8999f49a391782a544df5804
AlexanderSarl <alexander.sarleisky@yandex.ru>
2023-05-01 19:26:09 +0300

    Added a condition for the existence of an argument

--- fibonachi.cpp
+++ fibonachi.cpp
@@ -3,6 +3,11 @@
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "Rus");
+    if (argc < 2)
+    {
+        std::cerr << "No arguments specified!" << std::endl;
+        return 1;
+    }
     int next_number = 0, previous_number = 1;
     for (int i = 0; i < atoi(argv[1]); i++)
     {
